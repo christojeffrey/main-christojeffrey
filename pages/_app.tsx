3ade8d2b6d45c7149be512229e6acd19ebbd1af6
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2024-04-17 17:04:48 +0700

    add google anaytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,26 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Layout from "./layout";
+import Script from "next/script";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
+    <>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-CVE68XRBLW`} />
+
+      <Script strategy="lazyOnload" id="gtag">
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-CVE68XRBLW',{
+      page_path: window.location.pathname,
+      });
+     `}
+      </Script>
+
       <Component {...pageProps} />
-    </Layout>
+    </>
   );
 }
 
