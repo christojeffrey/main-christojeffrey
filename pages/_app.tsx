6217776d06c258de300092314c281137a67a00ed
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2024-04-17 19:22:09 +0700

    testing add google tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }: AppProps) {
   const googleAnalyticsID = "G-CVE68XRBLW";
   const microsoftClarityID = "lxzd3pv1ke";
+  const GTM_ID = "GTM-5QDW35T7";
   return (
     <>
       {/* google analytics */}
@@ -30,6 +31,21 @@ function MyApp({ Component, pageProps }: AppProps) {
           `}
       </Script>
 
+      {/* google tag manager */}
+      <Script id="gtm" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+      `}
+      </Script>
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;" />`,
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
