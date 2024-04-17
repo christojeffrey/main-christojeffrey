de27d177ff5ad751e06ff5fc23a05f5691eb239f
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2024-04-17 18:13:50 +0700

    feat: add clarity

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,33 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Script from "next/script";
-import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const googleAnalyticsID = "G-CVE68XRBLW";
+  const microsoftClarityID = "lxzd3pv1ke";
   return (
     <>
-      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-CVE68XRBLW`} />
+      {/* google analytics */}
+      <Script async src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsID}`} />
 
-      <Script strategy="lazyOnload" id="gtag">
+      <Script id="google-analytics">
         {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-CVE68XRBLW',{
-      page_path: window.location.pathname,
-      });
-     `}
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${googleAnalyticsID}');
+        `}
+      </Script>
+
+      {/* microsoft clarity */}
+      <Script type="text/javascript" id="microsoft-clarity">
+        {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${microsoftClarityID}");
+          `}
       </Script>
 
       <Component {...pageProps} />
